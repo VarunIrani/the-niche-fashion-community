533e5e4245f578853a1e9f052519884753a086fa
VarunIrani <varunirani0505@gmail.com>
2020-10-21 07:39:04 +0530

    Centered Logo on Phone and Tablet

--- src/components/NicheNavbar.jsx
+++ src/components/NicheNavbar.jsx
@@ -17,33 +17,38 @@ class NicheNavbar extends Component {
 							<Row style={{backgroundColor: COLORS.primary}} className='p-1'/>
 						</Container>
 						<Container className='my-3'>
-							<Row className='align-items-center justify-content-center'>
+							<Row className='align-items-center'>
 								<Hidden lgUp implementation="css" className="my-auto">
 									<Col xs="1" className='my-auto mr-1'>
 										<NicheDrawer/>
 									</Col>
 								</Hidden>
-								<Col xl='2' md='3' lg='2' xs={6}>
-									<Link to='/'>
-										<Image src={logo} style={{width: '100%'}}/>
-									</Link>
-								</Col>
-								<Col>
-									<Hidden mdDown implementation="css">
-										<Row className='justify-content-end'>
-											{ROUTES.map((route, index) =>
-												<Link to={route.path} key={index} style={{
-													fontFamily: 'Poppins',
-													fontWeight: 600,
-													fontSize: 16,
-													textDecoration: 'inherit',
-													color: 'inherit'
-												}} className='my-auto mr-4'>
-													{route.name}
-												</Link>
-											)}
-										</Row>
-									</Hidden>
+								<Col className='p-0 mr-5 mr-xl-0'>
+									<Row className='justify-content-center align-items-center justify-content-xl-around'>
+										<Col xl='2' md='3' lg='2' xs={6} className='p-0 pr-3 pr-xl-0'>
+											<Link to='/'>
+												<Image src={logo} style={{width: '100%'}}/>
+											</Link>
+										</Col>
+										<Hidden mdDown implementation="css">
+										<Col>
+												<Row className='justify-content-end'>
+													{ROUTES.map((route, index) =>
+														<Link to={route.path} key={index} style={{
+															fontFamily: 'Poppins',
+															fontWeight: 600,
+															fontSize: 16,
+															textDecoration: 'inherit',
+															color: 'inherit'
+														}} className='my-auto mr-4'>
+															{route.name}
+														</Link>
+													)}
+												</Row>
+
+										</Col>
+										</Hidden>
+									</Row>
 								</Col>
 							</Row>
 						</Container>
